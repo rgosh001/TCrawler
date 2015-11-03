2124c6815dc8a339e6f9b42470687f58af5c9b2b
Rashid Goshtasbi <rgosh001@ucr.edu>
2015-11-02 21:05:08 -0800

    added user file download functionality, appended link titles into json, fixed newline output for eat tweet data

--- tStream.py
+++ tStream.py
@@ -7,14 +7,14 @@ import json
 import os
 import sys
 import random
+import requests
 from bs4 import BeautifulSoup
 import re
 import urllib
 import urllib2
-import bs4
-
 #path for files to save
-path = "/Users/RashidGoshtasbi/tweets"
+#path = "/Users/RashidGoshtasbi/tweets"
+path = os.getcwd()
 
 #5GB required for downloads
 finalSize = 500
@@ -28,51 +28,78 @@ csecret="t3IFbnMdMnHx9oG0brfsLkuEh44nzsyRUsl3RsWPTyoGcYo2HB"
 atoken="32316013-GPuYYrD0XtPL0GBJHGdAvCQKEwaOxYbF0I0E4Xrp7"
 asecret="RP0tJAc9rYnQssJkT6EXfCActzLbxfLoiWcX62IbrE2n0"
 
-class listener(StreamListener):
+
+
+class listener(StreamListener):	
 	def on_data(self, data):
+		global url_text
 		global numFile
 		global finalSize
+		global tweet 
+		#global text
+		global text_tweet
+		global str1
+		global link_text
 		try:
 			with open("tweets"+str(numFile)+".txt", 'a') as output:
 				if(os.path.getsize("tweets"+str(numFile)+".txt") < 10000000):
-					# store this into anoother variable
-					# parse and get the link
-					# fetch the title of the webpage
 					# tweet[u'linktitle']=title
 					
+					#LOADS ALL OF THE TWEET DATA INTO VARIABLE "TWEET"
 					tweet = json.loads(data)
-					text_tweet = tweet[u'text'] #THIS IS THE TEXT ONLY OF THE TWEET
-					print text_tweet
+					
+					
+					#THIS IS THE TEXT ONLY OF THE TWEET
+					text_tweet = tweet[u'text']
+					
+					str1 = "http"
 										
-#					#use tweet_text to get url and save it into variable and enter below
-#					soup = BeautifulSoup(urllib2.urlopen(link_text))
-#					link_title = soup.title.string
-#					
-#					#WRITES ORIGINAL TEXT INTO FILE
+					#IF STATEMENTS LOOKS FOR TWEETS THAT HAVE TITLE
+					#IF IT DOESNT HAVE A URL, IT DOESNT PARSE IT
+					if (text_tweet.find('http') != -1):
+						url_text = text_tweet.find(str1)
+						link_text = text_tweet[url_text:url_text+23]
+						#print link_text
+						
+						content = urllib2.urlopen(link_text).read()
+						soup = BeautifulSoup(content, "html.parser")
+						tweet[u'linktitle']= soup.title.string
+					else:
+						pass
+						
+					#text = json.loads(tweet)
+					#WRITES ORIGINAL TEXT INTO FILE
+					#print json.dumps(tweet)
+					
+#					json.dump(tweet + '\n',output)
+					out = json.dumps(tweet)
+					output.write(out + '\n')
+
 #					output.write(tweet)
-#					output.write('\n')
 #					output.close()
-					
 					return(True)
-					
 				else:
 					if (numFile < finalSize):
 						numFile += 1
 					else:
 						os._exit(0)
-						
 		except BaseException, e:
 			print 'failed,', str(e)
 			#time.sleep(1)
 
-	def on_error(self, status):
-		print status
-		
+#	def on_error(self, status):
+#		print status
+
 if __name__ == '__main__':
 
 	l = listener()
 	auth = OAuthHandler(ckey, csecret)
 	auth.set_access_token(atoken, asecret)
-	twitterStream = Stream(auth, l)
-	twitterStream.filter(locations=[-180,-90,180,90], languages=["en"])
+	
+	while True:
+		try:
+			twitterStream = Stream(auth, l)
+			twitterStream.filter(locations=[-180,-90,180,90], languages=["en"])
+		except:
+			continue
 
